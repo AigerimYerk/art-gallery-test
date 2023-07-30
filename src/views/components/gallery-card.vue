185cad281d30e8a54ed37255a796fdff021d1342
AigerimYerk <aigerim@lessons.kz>
2023-07-31 00:00:18 +0600

    Created card for one picture

---
+++ src/views/components/gallery-card.vue
@@ -0,0 +1,27 @@
+<template>
+  <li class="gallery__item">
+    <router-link :to="this.link">
+      <img 
+        :src="item.urls.small"
+        :style="{'background-color':item.color}"
+        :alt="item.alt_description"
+        >
+      <span>{{ item.alt_description }}</span>
+    </router-link>
+  </li>
+</template>
+
+<script>
+
+export default ({
+  name: 'gallery-card',
+  props: {
+    item: Object,
+  },
+  data() {
+    return {
+      link: '/pictures/'+ String(this.item.id),
+    }
+  }
+})
+</script>
